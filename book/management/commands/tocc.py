46d6a772b3c607d14a71dab6047432e929e2bcc8
Saeed <hassanisaeed19@yahoo.com>
2021-10-23 00:06:13 +0330

    feat: retrieve list of nodes

--- book/management/commands/tocc.py
+++ book/management/commands/tocc.py
@@ -3,6 +3,8 @@ import json
 from django.core.management.base import BaseCommand
 from django.core.serializers.json import Serializer
 
+from book.models import Book, City
+
 
 class Command(BaseCommand):
 
@@ -17,19 +19,43 @@ class Command(BaseCommand):
         writer = csv.DictWriter(output, fieldnames=header, dialect='excel', delimiter=',')
         writer.writeheader()
 
-        for index, item in enumerate(data):
+        for item in data:
             row = [item[i] for i in header]
             writer.writerow(dict(zip(header, row)))
 
     def handle(self, *args, **options):
         from book.models import Book
+        from book.models import Publisher
 
-        obj = Book.objects.all()
-        data = JSONSerializer().serialize(obj, fields=["title", "description"])
+        book = Book.objects.all()
+        # for item in book:
+        #     print(item)
+        data = JSONSerializer().serialize(book, fields=['title', 'description', 'publisher'])
         self.render_to_excel(data)
 
 
+def get_field_name_types(self):
+    attributes = dir(self)
+    type_names = [eval("type(self." + dir(self)[i] + ").__name__") for i in range(len(dir(self)))]
+    item = list(set([attributes[i] if type_names[i] == 'ForeignKeyDeferredAttribute'
+                     else '' for i in range(len(attributes))]))
+    item.remove('')
+    return item[0] if len(item) > 0 else None
+
+
+def graph(node):
+    print([item.to_excel() for item in node])
+
+
 class JSONSerializer(Serializer):
-    def get_dump_object(self, obj):
+    def get_dump_object(self, obj, node=[]):
+        node.append(obj)
+        fk = get_field_name_types(obj.__class__)
+        if fk is not None:
+            attr = fk.split('_')[0]
+            self.get_dump_object(eval(f'obj.{attr}'))
+        else:
+            graph(node)
+            node.clear()
         self._current[obj._meta.pk.name] = obj._get_pk_val()
         return self._current
--- book/models.py
+++ book/models.py
@@ -1,10 +1,35 @@
 from django.db import models
 
 
+class City(models.Model):
+    name = models.CharField(max_length=40)
+
+    def to_excel(self):
+        return self.name
+
+    def __str__(self):
+        return self.name
+
+
+class Publisher(models.Model):
+    name = models.CharField(max_length=40)
+    city = models.ForeignKey(City, null=True, on_delete=models.CASCADE, related_name='publishers')
+
+    def to_excel(self):
+        return self.name
+
+    def __str__(self):
+        return self.name
+
+
 class Book(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
     last_modified = models.DateTimeField(auto_now_add=True)
+    publisher = models.ForeignKey(Publisher, on_delete=models.CASCADE, related_name='books')
+
+    def to_excel(self):
+        return self.title
 
     def __str__(self):
         return self.title
